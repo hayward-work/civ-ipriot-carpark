10059f2ba64644c41753518fa5e14974212ad7df
20125011 <20125011@tafe.wa.edu.au>
2025-06-04 14:16:42 +0800

    feat: implement class 'carPark'

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, all_cars=None, sensors=None,  displays=None):
+        self.location = location
+        self.capacity = sensors or []
+        self.displays = displays or []
+        self.all_cars = all_cars or {}
+
+    def __str__(self):
+        return f"{self.location} car park with {self.capacity} bays."
