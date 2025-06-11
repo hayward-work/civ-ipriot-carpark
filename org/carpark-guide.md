134edc4e7e1ab15570d8211ac395d954b5f19186
20125011 <20125011@tafe.wa.edu.au>
2025-06-11 14:23:15 +0800

    test: add unit tests and supporting screenshots for display class

--- org/carpark-guide.md
+++ org/carpark-guide.md
@@ -760,9 +760,10 @@ Next, we'll create tests for the `Display` class. These tests will test the `__i
 
 1. Add a screenshot of the output of the unit tests. If any failed, add a screenshot of the error message and a screenshot after you have fixed the errors:
 
-   ```markdown
-   ![Unit tests](images/unit-tests-display.png)
-   ```
+   ![Failed unit tests](images/failed_unit_tests_display.png)
+
+   ![Passed unit tests](images/passed_unit_tests_display.png)
+
 
 2. Commit your changes to the local repository. Tag the commit with `s7` so your lecturer can find it.
 3. Push the tag to the remote repository.
---
+++ tests/test_display.py
@@ -0,0 +1,19 @@
+import unittest
+from display import Display
+from car_park import CarPark
+
+class TestDisplay(unittest.TestCase):
+    def setUp(self):
+        self.car_park = CarPark("nowhere", 1)
+        self.display = Display(1, self.car_park, "Welcome to the car park", is_on=True)
+
+    def test_display_initialized_with_all_attributes(self):
+        self.assertIsInstance(self.display, Display)
+        self.assertEqual(self.display.id, 1)
+        self.assertEqual(self.display.message, "Welcome to the car park")
+        self.assertEqual(self.display.is_on, True)
+        self.assertIsInstance(self.display.car_park, CarPark)
+
+    def test_update(self):
+        self.display.update({"message": "Goodbye"})
+        self.assertEqual(self.display.message, "Goodbye")
